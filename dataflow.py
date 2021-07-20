a709bb2420475fd27bedcf39c24a50981236d7b2
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-20 18:11:13 -0400

    Add DataChain class

--- dataflow.py
+++ dataflow.py
@@ -14,3 +14,21 @@ import pyvis
 import numpy as np
 import matplotlib.pyplot as plt
 import uuid
+# TODO: use with arbitrary Python code/call graphs/abstract syntax trees?
+
+
+# In[212]:
+
+
+class DataChain:
+    def placeholder(self):
+        pass
+    p = placeholder
+    
+#     def __getitem__
+    
+    def __init__(self, *values, **namedvalues):
+        self.values = list(values)
+        self.namedvalues = namedvalues
+#         self.aux
+        self.nodes = []
